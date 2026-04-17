3937048d10d3c56c24439f2eed7450094e866fb5
rjapon <richardjv666@gmail.com>
2026-04-17 10:05:00 -0500

    UPD: Save

--- src/complejidades/ComplejidadConstante.java
+++ src/complejidades/ComplejidadConstante.java
@@ -1,4 +1,5 @@
-public class App {
+package complejidades;
+public class ComplejidadConstante {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("ola");
